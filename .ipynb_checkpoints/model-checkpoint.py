b666ec61d5c092245ddd128b7bb6deb4afbd001b
Paulyna Magana <paxpm8@nottingham.ac.uk>
2021-11-10 18:42:01 +0000

    Initial commit

---
+++ .ipynb_checkpoints/model-checkpoint.py
@@ -0,0 +1,7 @@
+# Importing necessary libraries
+import pandas as pd # To manage data as data frames
+import numpy as np # To manipulate data as arrays
+from sklearn.linear_model import LogisticRegression # Classification model
+
+# Importing the dataset
+data = pd.read_csv('./iris.csv')
